c9bf2dbfd5c9a2fd4136f46c05aceeb5b52272bd
tvsmith018 <104206007+tvsmith018@users.noreply.github.com>
2024-04-15 19:46:54 -0400

    update

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,8 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+# DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('SYSTEM_ALLOWED_HOSTS').split(" ")
 
